666382f66f6f2d4c8ab9eab6bd04f6efe41ec768
Qiuyang Nie <qyang.nie@gmail.com>
2020-10-09 20:28:01 +0100

    Scaffold Product model

---
+++ OrdersManagement/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrdersManagement.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ExpiredDate { get; set; }
+    }
+}
