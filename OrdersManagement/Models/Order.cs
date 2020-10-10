bd7b4655186835e7eca31f68e0bb12f5b0d017c7
Qiuyang Nie <qyang.nie@gmail.com>
2020-10-10 12:11:40 +0100

    Add Order model

---
+++ OrdersManagement/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrdersManagement.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime EstimateDate { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
