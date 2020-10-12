66bae5e189a2f68a72130424e9276ab6c0c39c84
Qiuyang Nie <qyang.nie@gmail.com>
2020-10-12 13:44:50 +0100

    Add customer id filter

--- OrdersManagement/Models/OrderViewModel.cs
+++ OrdersManagement/Models/OrderViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
 namespace OrdersManagement.Models
 {
     public class OrderViewModel
     {
-        public OrderViewModel()
-        {
-        }
+        public List<Order> Orders { get; set; }
+        public SelectList CustomerIds { get; set; }
+        public int CustomerId { get; set; }
     }
 }
